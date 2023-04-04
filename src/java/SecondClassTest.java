0e6d8006531dee51df481cd113710f43dd3e949b
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-04 17:30:06 +0300

    initial commit2

--- src/java/SecondClassTest.java
+++ src/java/SecondClassTest.java
@@ -1,5 +1,7 @@
 public class SecondClassTest {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        String str = "Hello GitHub again!";
+        System.out.println(str);
     }
 }
